6a2f789a57324b434dc10f6621658debc6171282
Dhananjay Borse <dhananjayborse001@gmail.com>
2026-07-28 18:00:39 +0530

    fix010

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="background-color: #ffffff;">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="background-color: #ffffff; color-scheme: light;">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="color-scheme" content="light">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
